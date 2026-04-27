8930c5d9f521c99930b0828a0ff9f0e505437b9d
arfdev74 <arfdevapps@outlook.com>
2026-04-27 19:48:50 -0300

    feat: APK gerado e configuracao Play Store

---
+++ app/src/main/java/br/com/appstools/duto/DelegationService.java
@@ -0,0 +1,14 @@
+package br.com.appstools.duto;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
